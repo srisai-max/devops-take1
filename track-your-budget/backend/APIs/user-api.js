66a36388ecfd6a5a011f8ef8f8fa5b8eb8b6d684
Srisaiganesh <srisaiganeshkommineni@gmail.com>
2025-11-15 11:09:34 +0530

    Update user-api.js

--- track-your-budget/backend/APIs/user-api.js
+++ track-your-budget/backend/APIs/user-api.js
@@ -12,7 +12,7 @@ userapp.use((req, res, next) => {
   purchasehistory = req.app.get('purchasehistory')
   next()
 })
-//triggering testings
+//triggering testing
 //create a user
 userapp.post('/user', expressAsyncHandler(async (req, res) => {
   let usercred = req.body;
